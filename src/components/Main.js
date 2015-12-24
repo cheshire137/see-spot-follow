dbf6e903c15f0afa5b55d7ea6c86bb9198c213c1
Sarah Vessels <cheshire137@gmail.com>
2015-12-24 12:48:51 -0500

    Add some structure from Materialize, use scss as main style file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 require('jquery');
 require('materialize-css/sass/materialize.scss');
 require('normalize.css');
-require('styles/App.css');
+require('styles/app.scss');
 require('materialize-css/js/materialize.js');
 
 import React from 'react';
@@ -9,11 +9,20 @@ import React from 'react';
 class AppComponent extends React.Component {
   render() {
     return (
-      <p>
-        <a href="https://accounts.spotify.com/authorize">
-          Sign in to Spotify
-        </a>
-      </p>
+      <div className="container">
+        <div className="section">
+          <h1 className="header">See Spot Follow</h1>
+          <div className="row">
+            <div className="col s12 m4">
+              <p>
+                <a href="https://accounts.spotify.com/authorize">
+                  Sign in to Spotify
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
