983e7ea7f4276a2a1bf5ce5e7f2e9a9e05c8a288
Sarah Vessels <cheshire137@gmail.com>
2015-12-24 12:44:26 -0500

    Add link to Spotify sign in for web flow

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,15 +6,14 @@ require('materialize-css/js/materialize.js');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <p>
+        <a href="https://accounts.spotify.com/authorize">
+          Sign in to Spotify
+        </a>
+      </p>
     );
   }
 }
